70e4e754f20eadfe6ef064d505500b52f29ea716
Эля <eleonora.brueva@yandex.by>
2025-12-18 18:34:52 +0300

    Исправила стиль кода: добавила пустую строку перед классом

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -7,11 +7,7 @@ import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-/*
-этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
-    ему нужны методы по загрузке списка слов из файла по имени файла
-    на выходе должен быть класс WordleDictionary
- */
+
 public class WordleDictionaryLoader {
 
     private final PrintWriter log;
